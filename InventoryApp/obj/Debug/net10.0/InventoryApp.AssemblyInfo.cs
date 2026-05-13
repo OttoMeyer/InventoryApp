8f683a18435677915e1d613e5ae1abda7ae171f1
OttoMear <zgmgtvz@gmail.com>
2026-05-13 16:13:41 +0800

    Add Inventory Page

--- InventoryApp/obj/Debug/net10.0/InventoryApp.AssemblyInfo.cs
+++ InventoryApp/obj/Debug/net10.0/InventoryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d307d7c626ccb2ee31cbaea31fdbb8828221c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5597c572844c183c176c23ba86f57a57817935e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
